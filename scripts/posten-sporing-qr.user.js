4bfdd41c132b6855e31c80736f9cb00a2c23064c
Alex Thomassen <alex@vangenplotz.no>
2023-10-27 14:04:46 +0200

    [Scripts] Posten/Bring QR Code - Add support for new beta tracking page

--- scripts/posten-sporing-qr.user.js
+++ scripts/posten-sporing-qr.user.js
@@ -1,28 +1,58 @@
 // ==UserScript==
 // @name        Posten/Bring Sporing - QR Kode
 // @namespace   github.com/Decicus
-// @version     1.4.2
+// @version     1.5.0
 // @match       https://sporing.posten.no/sporing/*
 // @match       https://sporing.bring.no/sporing/*
+// @match       https://beta.sporing.posten.no/sporing/*
+// @match       https://beta.sporing.bring.no/sporing/*
 // @grant       none
 // @downloadURL https://gist.githubusercontent.com/Decicus/0bfe36921a5dd92e679c4eb8ba67264d/raw/PostenSporing.user.js
 // @updateURL   https://gist.githubusercontent.com/Decicus/0bfe36921a5dd92e679c4eb8ba67264d/raw/PostenSporing.user.js
 // ==/UserScript==
-function handleMutationsList(list, observer) {
-    const target = document.querySelector('.hw-wysiwyg > div > div');
-    if (!target) {
-        return;
-    }
-
-    observer.disconnect();
+function getQrUrl()
+{
     const url = new URL(window.location.href);
     const sporingsnummer = url.pathname.replace('/sporing/', '');
     const resolution = 125;
     const qrUrl = `https://api.qrserver.com/v1/create-qr-code/?size=${resolution}x${resolution}&data=${sporingsnummer}`;
+    return qrUrl;
+}
 
-    const html = `<div><small><b>QR Code</b></small><div class="hw-block hw-block--pb-smallest"><img src="${qrUrl}" style="text-align: center;" alt="QR Code"></div></div>`;
-    target.insertAdjacentHTML('beforeend', html);
+let qrAdded = false;
+function handleMutationsList(list, observer) {
+    if (qrAdded) {
+        return;
+    }
 
+    let isNewPage = false;
+    let target = document.querySelector('.hw-wysiwyg > div > div');
+    if (!target) {
+        target = document.querySelector('div[data-testid="parcel-history-accordion"]');
+        if (!target) {
+            return;
+        }
+
+        isNewPage = true;
+    }
+
+    if (observer) {
+        observer.disconnect();
+    }
+
+    let elementLocation = 'beforeend';
+    let html = `<div><small><b>QR Code</b></small><div class="hw-block hw-block--pb-smallest"></div></div>`;
+
+    if (isNewPage) {
+        elementLocation = 'beforebegin';
+        html = `<div class="hw-grid__item hw-large--one-half mt-medium-4 large:mt-large-1"><h2>QR Code</h2><div class="mt-small-2 large:mt-small-3"><img src="${getQrUrl()}" title="Scan with the Posten app" alt="QR Code"></div></div>`;
+        // Padding between QR code and "Pakkehistorikk"
+        html += '<div class="hw-grid__item"></div>';
+        target = target.parentElement;
+    }
+
+    target.insertAdjacentHTML(elementLocation, html);
+    qrAdded = true;
     console.log('Added QR code');
 }
 
@@ -31,3 +61,7 @@ observer.observe(document.querySelector('main'), {
     subtree: true,
     childList: true,
 });
+
+setTimeout(() => {
+    handleMutationsList();
+}, 1000);
