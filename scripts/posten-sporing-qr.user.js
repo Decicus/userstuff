6f03662a47e1d3c04b9833e637d37d4390fdf197
Alex Thomassen <alex@thomassen.xyz>
2024-08-01 09:38:39 +0200

    [Scripts] Posten/Bring QR Code - Make script compatible with latest tracking pages

--- scripts/posten-sporing-qr.user.js
+++ scripts/posten-sporing-qr.user.js
@@ -1,11 +1,9 @@
 // ==UserScript==
 // @name        Posten/Bring Sporing - QR Kode
 // @namespace   github.com/Decicus
-// @version     1.5.2
+// @version     1.6.0
 // @match       https://sporing.posten.no/sporing/*
 // @match       https://sporing.bring.no/sporing/*
-// @match       https://beta.sporing.posten.no/sporing/*
-// @match       https://beta.sporing.bring.no/sporing/*
 // @grant       none
 // @downloadURL https://gist.githubusercontent.com/Decicus/0bfe36921a5dd92e679c4eb8ba67264d/raw/PostenSporing.user.js
 // @updateURL   https://gist.githubusercontent.com/Decicus/0bfe36921a5dd92e679c4eb8ba67264d/raw/PostenSporing.user.js
@@ -27,30 +25,15 @@ function handleMutationsList(list, observer) {
     }
 
     let isNewPage = false;
-    let target = document.querySelector('.hw-wysiwyg > div > div');
-    if (!target) {
-        target = document.querySelector('div[data-testid="parcel-history-accordion"]');
-        if (!target) {
-            return;
-        }
-
-        isNewPage = true;
-    }
+    let target = document.querySelector('*[data-testid="trackingnumber-main-header"]');
 
     if (observer) {
         observer.disconnect();
     }
 
-    let elementLocation = 'beforeend';
-    let html = `<div id="${elementId}"><small><b>QR Code</b></small><div class="hw-block hw-block--pb-smallest"><img src="${getQrUrl()}" title="Scan with the Posten app" alt="QR Code"></div></div>`;
-
-    if (isNewPage) {
-        elementLocation = 'beforebegin';
-        html = `<div id="${elementId}" class="hw-grid__item hw-large--one-half mt-medium-4 large:mt-large-1"><h2>QR Code</h2><div class="mt-small-2 large:mt-small-3"><img src="${getQrUrl()}" title="Scan with the Posten app" alt="QR Code"></div></div>`;
-        // Padding between QR code and "Pakkehistorikk"
-        html += '<div class="hw-grid__item"></div>';
-        target = target.parentElement;
-    }
+    target = target.parentElement.parentElement;
+    const elementLocation = 'afterend';
+    const html = `<div id="${elementId}" class="empty:hidden has-[h2:last-child]:hidden [&>*:not(:first-child)]:mt-20 large:[&>*:not(:first-child)]:mt-24"><h2>QR Code</h2><img src="${getQrUrl()}" title="Scan with the Posten app" alt="QR Code"></div>`;
 
     target.insertAdjacentHTML(elementLocation, html);
     console.log('Added QR code');
